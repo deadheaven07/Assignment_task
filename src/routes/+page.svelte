381f258979f75ae2fff98209b050c74707e70162
Harsh Raghuwanshi <117965451+deadheaven07@users.noreply.github.com>
2024-03-12 22:42:13 +0530

    Add files via upload

---
+++ src/routes/+page.svelte
@@ -0,0 +1,187 @@
+<header class="border-b-2 border-black border-dotted">
+    <nav
+        class="flex items-center justify-between px-4 py-2 bg-white"
+        style="padding: 10px, 0px, 0px, 0px;"
+    >
+        <div class="flex items-center justify-center">
+            <img
+                src="/duggup_logo.jpeg"
+                class="h-6 w-6 text-blue-700"
+                alt="duggup_logo"
+            />
+            <span
+                class="self-center text-6x6 font-bold whitespace-nowrap text-black ml-2"
+                >duggup</span
+            >
+        </div>
+
+        <div class="flex items-center space-x-2">
+            <a
+                href="/"
+                class="text-gray-900 flex items-center justify-center hover:text-blue-700 dark:text-black justify-self-end"
+                ><img
+                    src="/Container.jpg"
+                    class="h-6 w-6 text-blue-700"
+                    alt="Contain"
+                /> <span>Home</span></a
+            >
+            <a
+                href="/explore"
+                class="text-gray-900 flex items-center justify-center hover:text-blue-700 dark:text-black justify-self-end"
+                ><img
+                    src="/Container1.jpg"
+                    class="h-6 w-6 text-blue-700"
+                    alt="Contain"
+                /> <span>Explore</span></a
+            >
+            <a
+                href="/post"
+                class="text-gray-900 flex items-center justify-center hover:text-blue-700 dark:text-black justify-self-end"
+                ><img
+                    src="/Container2.jpg"
+                    class="h-6 w-6 text-blue-700"
+                    alt="Contain"
+                /> <span>Learn</span></a
+            >
+            <a
+                href="/post"
+                class="text-gray-900 flex items-center justify-center border-2 p-0 hover:text-blue-700 dark:text-black justify-self-end"
+                ><img
+                    src="/Container3.jpg"
+                    class="h-6 w-6 text-blue-700"
+                    alt="Contain"
+                /> <span>Post</span></a
+            >
+
+            <!-- Dropdown -->
+            <div class="relative justify-self-end">
+                <button
+                    type="button"
+                    data-dropdown-toggle="language-dropdown-menu"
+                    class="text-gray-900 flex justify-center hover:text-blue-700 items-center dark:text-black justify-self-end"
+                    ><img
+                        src="/Image.jpg"
+                        class="h-4 w-auto rounded-full border border-black"
+                        alt="duggup_logo"
+                    />
+                    <span>Krishna Kiran</span>
+                </button>
+            </div>
+            <img
+                src="/chevrondown.png"
+                class="h-4 item-centre flex w-auto rounded-full border border-black"
+                alt="duggup_logo"
+            />
+        </div>
+    </nav>
+</header>
+
+<div class="container mx-auto py-8 text-left">
+    <!-- Your content goes here -->
+    <div class="flex justify-center items-center gap-5">
+        <div class="">
+            <img
+                src="/krishna kiran.jpg"
+                class="h-4 w-4 mb-4 rounded-full aspect-square border-2"
+                alt="krishna kiran"
+                style="height: 75px; width: auto "
+            />
+            <p class="font-serif pb-4">Krishna Kiran</p>
+        </div>
+        <div>
+            <p class="w-[36rem] text-sm">
+                Co-Founder and CEO at Duggup - Social network for people in
+                tech. Ex-Amazon Head of Engineering. I write hot-takes on
+                building a business , shipping delightful products and
+                accelerating product and career growth.
+            </p>
+            <button
+                class="rounded w-25 aspect-rectangle drop-shadow-xl border-2 border-slate-300"
+                >Followed</button
+            >
+        </div>
+        <div>
+            <img
+                src="/duggup_logo.jpeg"
+                class="h-6 w-6 mb-4 rounded-full"
+                alt="duggup_logo"
+                style="height: 40px; width: auto"
+            />
+            <p class="text-sm">Duggup</p>
+            <p class="text-xs">Co-Founder and CEO</p>
+        </div>
+    </div>
+
+    <div class="flex justify-center items-center gap-32">
+        <div class="w-56">
+            <img
+                src="/steve jobs.jpg"
+                class="h-1 w-auto mb-4"
+                alt="steve jobs"
+                style="height: 100px; width: auto"
+            />
+            <p>
+                No amount of technology can convert a bad story into a good
+                story
+            </p>
+        </div>
+        <div class="w-56">
+            <img
+                src="/sam altman.jpeg"
+                class="h-1 w-auto mb-4"
+                alt="sam altman"
+                style="height: 100px; width: auto"
+            />
+
+            <p>
+                Most people don't have original ideas. Here s how Sam Altman
+                pushes himself to have...
+            </p>
+        </div>
+    </div>
+<div class="flex justify-center items-center gap-96">
+    <img
+        src="/duggup_logo.jpeg"
+        class="h-1 w-auto mb-4 "
+        alt="duggup logo"
+        style="height: 35px; width: auto"
+    />
+   <div>
+    <h1>Co-Founder and CEO</h1>
+    <p>Full-time Remote</p>
+</div>
+</div>
+<div class="flex justify-center items-center">
+   <div class="w-56"> 
+    <img
+        src="/desktop.jpg"
+        class="h-1 w-auto mb-4"
+        alt="desktop"
+        style="height: 100px; width: auto"
+    />
+    <p>Startup lesson I am reflecting. Don't build for the "average person".</p>
+</div>
+<div class="w-56">
+    <img
+        src="/tokyo bay map.jpg"
+        class="h-1 w-auto mb-4"
+        alt="tokyo bay map"
+        style="height: 100px; width: 170px"
+    />
+
+    <p>Your biggest regret at 80 will be acts of omission.</p>
+</div>
+</div>
+<div class="flex justify-center items-center gap-96">
+    <img
+        src="/betterup_logo.jpeg"
+        class="h-1 w-auto mb-4"
+        alt="betterup logo"
+        style="height: 40px; width: auto"
+    />
+    <div>
+    <h1>VP Engineering</h1>
+    <p>Full-time</p>
+</div>
+</div>
+</div>
